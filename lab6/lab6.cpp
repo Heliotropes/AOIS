bd1d5c16da9b1ed8eec195249352a86b1632167e
Vlad Maevski <93771100+Heliotropes@users.noreply.github.com>
2023-06-21 03:01:18 +0300

    Add files via upload

---
+++ lab6/lab6.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int GetHash(string key);
+void Add(string key, string value);
+void Search(string key);
+void Delete(string key);
+void Print();
+
+
+struct HashTableElement
+{
+	int id;
+	string key;
+	string value;
+	int colision;
+
+	HashTableElement()
+	{
+		key = "";
+		value = "";
+		id = 0;
+		colision = 0;
+	}
+
+	HashTableElement(string _key, string _value)
+	{
+		key = _key;
+		value = _value;
+		id = GetHash(key);
+		colision = 0;
+	}
+};
+
+vector <HashTableElement> Table(174);
+
+int main()
+{
+	while (1)
+	{
+		Print();
+		string key, value;
+		cout << "\n\n1) - Add\n2) - Search\n3) - Delete\n4) - Clear\n";
+		int variant;
+		cin >> variant;
+		if (variant == 1) 
+		{
+			cin >> key >> value;
+			Add(key, value);
+		}
+		else if (variant == 2)
+		{
+			cin >> key;
+			Search(key);
+		}
+		else if (variant == 3)
+		{
+			cin >> key;
+			Delete(key);
+		}
+		else if (variant == 4) system("cls");
+		cout << "\n\n";
+	}
+}
+
+int GetHash(string key)
+{
+	int result = 0;
+	if (key.length() >= 3) result = int(key[0]) + int(key[1]) + int(key[2]) - 192;
+	else if (key.length() == 2) result = int(key[0]) + int(key[1])-128;
+	else if (key.length() == 1) result = int(key[0])-64;
+	return result;
+}
+
+void Add(string key, string value)
+{
+	HashTableElement currentElem(key, value);
+	if(Table[GetHash(key)  ].id == 0) Table[GetHash(key)  ] = currentElem;
+	else 
+	{
+		Table[GetHash(key)  ].colision = GetHash(key)  ;
+		int i = 1;
+		while (GetHash(key)   + i < Table.size() && Table[GetHash(key)   + i].id != 0)
+			++i;
+		Table[GetHash(key) + i] = currentElem;
+		Table[GetHash(key) + i].id = GetHash(key) + i;
+		Table[GetHash(key)   + i].colision = GetHash(key)  ;
+	}
+}
+
+void Search(string key)
+{
+	cout << Table[GetHash(key)  ].value;
+}
+
+void Delete(string key)
+{
+	HashTableElement currentElem("", "");
+	Table[GetHash(key) ] = currentElem;
+}
+void Print()
+{
+	int index = 0;
+	while (index < Table.size()) {
+		if (Table[index].id != 0)cout <<"\n" << Table[index].id << " " << Table[index].key << " " << Table[index].value;
+		if (Table[index].id != 0 && Table[index].colision != 0) cout << " *colision - " << Table[index].colision << "*";
+		++index;
+	}
+}
---
+++ lab7/lab7.cpp
@@ -0,0 +1,100 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector <bool> to_binary(int num)
+{
+	vector <bool> res;
+	int size;
+	int num_;
+	num_ = abs(num);
+	while (num_)
+	{
+		res.emplace(res.begin(), num_ % 2);
+		num_ /= 2;
+	}
+	size = res.size();
+	for (size_t i = 0; i < 16 - size; i++)
+	{
+		res.emplace(res.begin(), 0);
+	}
+	return res;
+}
+
+vector <bool> nearest_from_top(vector<vector<bool>> matr, vector<bool> val)
+{
+	vector <bool> min;
+	vector<vector<bool>> top_list;
+	for (int i = 0; i < matr.size(); ++i)
+		if (matr[i] > val) top_list.push_back(matr[i]);
+	if (!top_list.empty())
+	{
+		min = top_list[0];
+		for (int i = 0; i < top_list.size(); ++i)
+			if (top_list[i] < min) min = top_list[i];
+	}
+	return min;
+}
+vector <bool> nearest_from_bot(vector<vector<bool>> matr, vector<bool> val)
+{
+	vector <bool> max;
+	vector<vector<bool>> bot_list;
+	for (int i = 0; i < matr.size(); ++i)
+		if (matr[i] < val) bot_list.push_back(matr[i]);
+	if (!bot_list.empty())
+	{
+		max = bot_list[0];
+		for (int i = 0; i < bot_list.size(); ++i)
+			if (bot_list[i] > max) max = bot_list[i];
+	}
+	return max;
+}
+
+vector <vector <bool>> interval(vector<vector<bool>> matr, vector<bool> val1, vector<bool> val2)
+{
+	vector<vector<bool>> list;
+	for (int i = 0; i < matr.size(); ++i)
+		if (matr[i] > val1 && matr[i] < val2) list.push_back(matr[i]);
+	return list;
+}
+void print(vector<vector<bool>> matr)
+{
+	for (int i = 0; i < matr.size(); ++i)
+	{
+		for (int j = 0; j < matr[i].size(); ++j)
+			cout << matr[i][j] << " ";
+		cout << "\n";
+	}
+}
+void print(vector<bool> matr)
+{
+	for (int i = 0; i < matr.size(); ++i)
+			cout << matr[i] << " ";
+}
+
+int main()
+{
+	int digits[10] = { 232, 125, 1789, 999, 23191,44444, 70, 879, 9340, 11111 };
+	vector<vector<bool>> matrix;
+	for (int i = 0; i < 10; ++i)
+		matrix.push_back(to_binary(digits[i]));
+	vector<bool> bot = to_binary(3472);
+	vector<bool> top = to_binary(25000);
+	vector<bool> value = to_binary(10000);
+	print(matrix);
+	cout << "\n\nInterval between ";
+	print(bot);
+	cout << "and ";
+	print(top);
+	cout << "\n";
+	print(interval(matrix, bot, top));
+	cout << "\n\n\nNearest to ";
+	print(value);
+	cout << "\nfrom top: ";
+	print(nearest_from_top(matrix, value));
+	cout << "\nfrom bot: ";
+	print(nearest_from_bot(matrix, value));
+}
+
+	
